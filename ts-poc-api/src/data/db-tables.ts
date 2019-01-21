92aaa8946923a6164f743155e09d1abedef73abe
nivinjoseph <nivinjoseph@outlook.com>
2019-01-21 10:33:29 -0500

    refactorings

--- ts-poc-api/src/data/db-tables.ts
+++ ts-poc-api/src/data/db-tables.ts
@@ -16,35 +16,9 @@ export class DbTables
 
     public async create(): Promise<void>
     {
-        const sql = `
-            create table todos
-            (
-                id varchar (48) primary key,
-                version int not null,
-                created_at bigint not null,
-                updated_at bigint not null,
-                data jsonb not null
-            );
-            
-            
-            create table todo_events
-            (
-                id varchar (50) primary key,
-                aggregate_id varchar (32) not null,
-                data jsonb not null
-            );
-            
-            create table todo_snaps 
-            (
-                id varchar (32) primary key,
-                data jsonb not null
-            );
-        `;
-
         try 
         {
-            await this._db.executeCommand(sql);
-            await this._db.executeCommand(`create index concurrently idx_todo_events__aggregate_id on todo_events(aggregate_id);`);
+            await this.createTodoTables();
         }
         catch (error)
         {
@@ -55,23 +29,58 @@ export class DbTables
 
     public async drop(): Promise<void>
     {
-        const sql = `
-            drop table todos;
-
-            drop table todo_events;
-            
-            drop table todo_snaps;
-        `;
-
         try 
         {
-            await this._db.executeCommand(`drop index concurrently idx_todo_events__aggregate_id;`);
-            await this._db.executeCommand(sql);
+            await this.dropTodoTables();
         }
         catch (error)
         {
             console.log("Tables drop failed!!", error);
             throw error;
         }
+    }
+    
+    private async createTodoTables(): Promise<void>
+    {
+        await this._db.executeCommand(`
+            create table todos
+            (
+                id varchar(48) primary key,
+                version int not null,
+                created_at bigint not null,
+                updated_at bigint not null,
+                data jsonb not null
+            );
+            
+            create table todo_events
+            (
+                id varchar(50) primary key,
+                aggregate_id varchar(32) not null,
+                data jsonb not null
+            );
+            
+            create table todo_snaps 
+            (
+                id varchar(32) primary key,
+                data jsonb not null
+            );
+        `);
+        
+        await this._db.executeCommand(`
+            create index concurrently idx_todo_events__aggregate_id on todo_events(aggregate_id);
+        `);
+    }
+    
+    private async dropTodoTables(): Promise<void>
+    {
+        await this._db.executeCommand(`drop index idx_todo_events__aggregate_id;`);
+        
+        await this._db.executeCommand(`
+            drop table todos;
+
+            drop table todo_events;
+            
+            drop table todo_snaps;
+        `);
     }
 }
