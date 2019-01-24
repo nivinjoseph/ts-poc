18e1cc29c1270c193737ba13b29e043c5b2d5480
nivinjoseph <nivinjoseph@outlook.com>
2019-01-24 02:54:45 -0500

    refactorings

--- ts-poc-api/src/api/controllers/controllers.ts
+++ ts-poc-api/src/api/controllers/controllers.ts
@@ -1,8 +1,8 @@
 import { GetAllTodosController } from "./queries/get-all-todos-controller";
 import { GetTodoController } from "./queries/get-todo-controller";
 import { CreateTodoController } from "./commands/create-todo-controller";
-import { UpdateTodoController } from "./commands/update-todo-command";
-import { MarkTodoAsCompletedController } from "./commands/mark-todo-as-completed";
+import { UpdateTodoController } from "./commands/update-todo-controller";
+import { MarkTodoAsCompletedController } from "./commands/mark-todo-as-completed-controller";
 import { DeleteTodoController } from "./commands/delete-todo-controller";
 
 
